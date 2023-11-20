138db7d1b56030df18026be164b07d53c19c137a
ivarovsky <ivarovskii@ukr.net>
2023-11-20 10:34:22 +0200

    Update monobank.class.php

--- monobank.class.php
+++ monobank.class.php
@@ -1,1 +1,11 @@
+<?php class monobank 
+  {
+    private $testing_mode = 1;
+    
+    private function check_signature($params){}
 
+    private function get_link($data){}
+    
+    public function create_payment_button($data){}
+    
+  }
