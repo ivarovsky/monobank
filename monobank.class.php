d7c66ad2fd30207614772bc27b142011a350accb
ivarovsky <ivarovskii@ukr.net>
2023-11-20 15:49:33 +0200

    Update monobank.class.php

--- monobank.class.php
+++ monobank.class.php
@@ -14,42 +14,36 @@ class monobank
     private $api_link = "https://api.monobank.ua/";
     private $redirectUrl = "";
     private $webHookUrl = "";
-    
+    private $_server_response_code = null;
     private function check_signature($params){}
-
-    private function send_data_to_monobank($params,$action,$key)
+	
+	    private function send_data_to_monobank($params,$action,$key)
     {
-        $curl = curl_init();
+		    if(is_array($params)) $params = json_encode($params);
+	      $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $this->api_link.$action);
-        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true); // Avoid MITM vulnerability http://phpsecurity.readthedocs.io/en/latest/Input-Validation.html#validation-of-input-sources
-        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);    // Check the existence of a common name and also verify that it matches the hostname provided
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, $timeout);   // The number of seconds to wait while trying to connect
-        curl_setopt($curl, CURLOPT_TIMEOUT, $timeout);          // The maximum number of seconds to allow cURL functions to execute
-        curl_setopt($curl, CURLOPT_POST, true);
+		    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
         curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, ["X-Token"=>$key]);
+		    curl_setopt($curl, CURLOPT_HTTPHEADER, ["X-Token: {$key}","Content-Type: application/json","Content-Length: ".strlen($params)]);
         $server_output = curl_exec($curl);
-        $this->_server_response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        curl_close($ch);
-        return $server_output;
-    }
-    
-    public function create_payment_button($data,$key)
+        $this->_server_response_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+    		return $server_output;
+	}
+	
+	   public function create_payment_button($data,$key)
     {
-     if(!isset($data['amount'])) {echo "Amount not set" exit();} //Check Required Field
+      if(!isset($data['amount'])) {echo "Amount not set"; exit();} //Check Required Field
       if(!isset($data['ccy'])) $data['ccy'] = 980;
-      if(!isset($data['redirectUrl'])) $data['redirectUrl'] =$this->redirectUrl;
-      if(!isset($data['webHookUrl'])) $data['webHookUrl'] =$this->webHookUrl;
-      if(!isset($data['validity'])) $data['validity'] =3600;
-      if(!isset($data['paymentType'])) $data['paymentType'] ="debit";
-
-      
-    $pay_link = send_data_to_monobank($data,"api/merchant/invoice/create",$key);
-    if(strlen($pay_link))  return "<a href='{$pay_link}' ><button>PAY</button></a>";
-    else return "Something went wrong!";
-    }
+      if(!isset($data['redirectUrl'])) $data['redirectUrl'] = $this->redirectUrl;
+      if(!isset($data['webHookUrl'])) $data['webHookUrl'] = $this->webHookUrl;
+      if(!isset($data['validity'])) $data['validity'] = 3600;
+      if(!isset($data['paymentType'])) $data['paymentType'] = "debit";
+      $pay_link = $this->send_data_to_monobank($data,"api/merchant/invoice/create",$key);
+	    $pay_link = json_decode($pay_link,1);
+	    if(isset($pay_link) || strlen($pay_link['pageUrl'])>2)  return "<a href='{$pay_link['pageUrl']}' ><button>PAY</button></a>";
+    else return json_encode($pay_link);
+	  }
     
   }
-
-
