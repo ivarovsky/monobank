34d04c239004121c4c8e370edcaf2453997cb2ec
ivarovsky <ivarovskii@ukr.net>
2023-11-20 16:03:00 +0200

    Update example.php

--- example.php
+++ example.php
@@ -6,10 +6,9 @@ $params = [
     "amount" => 4200,
     "ccy" => 980,
     "merchantPaymInfo" => [
-        "reference" => $data['reference'],
-        "destination" => $data['reference'],
-        "comment" => $data['comment'],
-        "customerEmails" => $data,
+        "reference" => "84d0070ee4e44667b31371d8f8813947",
+        "destination" => "Покупка щастя",
+        "comment" => "Покупка щастя",
         "basketOrder" => [
             [
                 "name" => "Табуретка",
@@ -18,18 +17,6 @@ $params = [
                 "icon" => "string",
                 "unit" => "шт.",
                 "code" => "d21da1c47f3c45fca10a10c32518bdeb",
-                "barcode" => "string",
-                "header" => "string",
-                "footer" => "string",
-                "tax" => [],
-                "uktzed" => "string",
-                "discounts" => [
-                    [
-                        "type" => "DISCOUNT",
-                        "mode" => "PERCENT",
-                        "value" => "PERCENT",
-                    ],
-                ],
             ],
         ],
     ],
@@ -38,14 +25,7 @@ $params = [
         "https://example.com/mono/acquiring/webhook/maybesomegibberishuniquestringbutnotnecessarily",
     "validity" => 3600,
     "paymentType" => "debit",
-    "qrId" => "XJ_DiM4rTd5V",
-    "code" => "0a8637b3bccb42aa93fdeb791b8b58e9",
-    "saveCardData" => [
-        "saveCard" => true,
-        "walletId" => "69f780d841a0434aa535b08821f4822c",
-    ],
 ];
 $xtoken = ""; //ADD YOUR TOKEN HERE
-
-create_payment_button($params,$xtoken);
-
+//LINK FOR TEST TOKEN https://api.monobank.ua/
+echo $monobank->create_payment_button($params,$xtoken);
